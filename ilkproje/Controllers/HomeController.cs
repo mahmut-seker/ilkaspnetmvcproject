ce78b9974d96c03e25582e10fab60098cf11e0d8
mahmut şeker <61181185+mahmut-seker@users.noreply.github.com>
2022-03-17 14:42:33 +0300

    yazar

--- ilkproje/Controllers/HomeController.cs
+++ ilkproje/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ilkproje.Models;
 
 namespace ilkproje.Controllers
 {
@@ -29,8 +30,19 @@ namespace ilkproje.Controllers
         public ActionResult test()
         {
             ViewBag.Message = "Your test page.";
-
-            return View();
+            ViewBag.yazarA = "orhan pamuk";
+            ViewBag.yazarBi = "Mimarlık bölümünü bırakan Pamuk, İstanbul Üniversitesi'nde Gazetecilik okudu. 23 yaşından sonra yazar olmaya karar verdi ve tüm uğraşlarını bir kenara koydu, eve kapanıp kendini yazmaya verdi. İlk romanı “Cevdet Bey ve Oğulları” 1982 tarihinde yayımlandı.";
+            ViewBag.tarih = DateTime.Now;
+            yazar yazar = new yazar()
+            {
+                ID = 1,
+                yazarAdi = "orhan",
+                yazarSoyad = "pamuk",
+                Biyografi = "Mimarlık bölümünü bırakan Pamuk, İstanbul Üniversitesi'nde Gazetecilik okudu. 23 yaşından sonra yazar olmaya karar verdi ve tüm uğraşlarını bir kenara koydu, eve kapanıp kendini yazmaya verdi. İlk romanı “Cevdet Bey ve Oğulları” 1982 tarihinde yayımlandı.",
+                dogumTarihi = DateTime.Parse("7 Haziran 1952")
+            };
+            return View(yazar);
         }
+       
     }
 }
---
+++ ilkproje/Models/yazar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ilkproje.Models
+{
+    public class yazar
+    {
+        public int ID { get; set; }
+        public string yazarAdi { get; set; }
+        public string yazarSoyad { get; set; }
+        public string Biyografi { get; set; }
+        public DateTime dogumTarihi { get; set; }
+    }
+}
